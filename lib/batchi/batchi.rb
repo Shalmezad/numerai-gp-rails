b48ca6d8097e65492898f3c359688a207bf173ee
Richard Wardin <richardw@hesonline.com>
2017-05-19 10:00:00 -0500

    More work on batchi. Need to figure out a good way to do callback...

--- lib/batchi/batchi.rb
+++ lib/batchi/batchi.rb
@@ -4,7 +4,43 @@ module BatChi
   end
 
   class Batch
+    NEXT_AVAILABLE_BATCH_ID_KEY="BatChi:next_available_batch_id"
 
+    def self.batch_count_key(batch_id)
+      "BatChi:batch_#{batch_id}_job_count"
+    end
+
+    def self.redis
+      # TODO: Eventually have our own redis:
+      return Resque.redis
+    end
+
+    def initialize
+      # Need to get the next available batch id:
+      @batch_id = Batch.redis.incr(NEXT_AVAILABLE_BATCH_ID_KEY)
+      @batch_jobs = []
+      @batch_callback = nil
+    end
+
+    def callback(clazz, *args)
+      @batch_callback = {}
+      @batch_callback[:clazz] = clazz
+      @batch_callback[:args] = args
+    end
+
+    def add(clazz, *args)
+      batch_job = {}
+      batch_job[:clazz] = clazz
+      batch_job[:args] = args
+      @batch_jobs << batch_job
+    end
+
+    def start
+      # Alright, we need to:
+      # 1) Set up our job counter:
+      Batch.redis.set(Batch.batch_count_key(@batch_id), @batch_jobs.count)
+      # 2) Set up and start the jobs:
+    end
   end
 
 end
