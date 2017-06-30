7a0d92c7791bc14909082d2ea5b8fff9ae5f120c
Richard Wardin <richardw@hesonline.com>
2017-06-30 10:32:36 -0400

    Can create demes from dashboard

--- app/controllers/demes_controller.rb
+++ app/controllers/demes_controller.rb
@@ -4,4 +4,15 @@ class DemesController < ApplicationController
     @deme = Deme.find(deme_id)
     @generationStats = @deme.generation_stats
   end
+
+
+  def new
+    @deme = Deme.new
+  end
+
+  def create
+    # We have our params:
+    CreateDemeJob.perform_later(params[:deme][:max_program_size].to_i)
+    redirect_to "/dashboard"
+  end
 end
